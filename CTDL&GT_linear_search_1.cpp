ac686a13bbbd142563db87cf7b10a41b21ac7138
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-06-10 01:36:10 +0700

    Update CTDL&GT_linear_search_1.cpp

--- CTDL&GT_linear_search_1.cpp
+++ CTDL&GT_linear_search_1.cpp
@@ -1,6 +1,3 @@
-//nguồn lập trình không khó
-#include <stdio.h>
- 
 int search(int arr[], int n, int x)
 {
   int i;
@@ -10,17 +7,4 @@ int search(int arr[], int n, int x)
       return i;
   // Nếu không tìm thấy trả về -1. Vì chỉ số mảng >= 0
   return -1;
-}
- 
-int main() {
-  int arr[] = {2, 3, 4, 10, 40};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  int x = 10;
-  int result = search(arr, n, x);
-  if (result != -1) {
-    printf("%d xuat hien tai chi so %d", x, result);
-  } else {
-    printf("%d khong co trong mang", x);
-  }
-  return 0;
 }
