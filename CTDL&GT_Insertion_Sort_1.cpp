8a4916e020e441a54253bad3676bfb59a1459fc1
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-06-10 00:08:09 +0700

    Update and rename CTDL&GT_Insertion_Sort_1.c to CTDL&GT_Insertion_Sort_1.cpp

---
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -0,0 +1,14 @@
+//nguồn viblo
+void Insertion_sort(int a[], int n) {
+	int index, new_number;
+	for (int i = 1; i < n; i++){
+		index = i;
+		new_number = a[i];
+		while (index > 0 && a[index - 1] > new_number){
+			a[index] = a[index - 1];
+			index--;
+		}
+		a[index] = new_number;
+	}
+}
+
