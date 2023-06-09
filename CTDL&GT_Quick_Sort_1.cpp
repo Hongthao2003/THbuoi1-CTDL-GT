59166f6719c23da032544f7032845c3f5843434c
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-06-10 01:34:58 +0700

    Update CTDL&GT_Quick_Sort_1.cpp

--- CTDL&GT_Quick_Sort_1.cpp
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -1,1 +1,19 @@
-
+void Quick_Sort(int a[], int left, int right){
+	int i, j, x;
+	x = a[(left+right)/2];
+	i = left; j = right;
+	do{
+		while(a[i] < x) i++;
+		while(a[j] > x) j--;
+		if(i <= j){
+			swap(a[i],a[j]);
+			i++; j--;
+		}
+	}while(i <= j);
+	if(left<j){
+		Quick_Sort(a, left, j);
+	}
+	if(i<right){
+		Quick_Sort(a, i, right);
+	}
+}
