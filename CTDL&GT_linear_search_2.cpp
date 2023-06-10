65d5559a1a4e81ea2881c430b0b2a0944962b7cf
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-06-10 16:06:45 +0700

    Update CTDL&GT_linear_search_2.cpp

--- CTDL&GT_linear_search_2.cpp
+++ CTDL&GT_linear_search_2.cpp
@@ -1,1 +1,22 @@
-tìm kiếm nhị phân tuyến tính là duyệt lần lượt từng phần từ của danh sách đó đến khi nào tìm được giá trị mong muốn hay đã duyệt hết qua hết danh sách.
+#include <iostream> 
+using namespace std; 
+  
+int search(int arr[], int n, int x) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        if (arr[i] == x) 
+            return i; 
+    return -1; 
+} 
+  
+int main(void) 
+{ 
+    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int x = 10; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int result = search(arr, n, x); 
+   (result == -1)? cout<<"Không có phần tử trong mảng"
+                 : cout<<"Phần tử có tại chỉ mục " <<result;
+   return 0; 
+} 
