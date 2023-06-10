6064744122dc822fa0d1d28c6a0b1c284ef79258
Hongthao2003 <125128510+Hongthao2003@users.noreply.github.com>
2023-06-10 15:55:30 +0700

    Create CTDL&GT_Heap_Sort_1.cpp

---
+++ CTDL&GT_Heap_Sort_1.cpp
@@ -0,0 +1,10 @@
+void max_heap_sort(int A[ ])
+{
+    int heap_size = N;
+    run_maxheap(A);
+    for(int i = N; i>=2 ; i-- )
+    {
+        swap(A[ 1 ], A[ i ]);
+        max_heap(A, 1, --heap_size);
+    }
+}
